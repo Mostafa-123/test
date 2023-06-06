8f2d60308bccf6dd00f3780ad54305d2f552a168
Omar <anaomar887@gmail.com>
2023-06-06 14:41:31 +0300

    get & check Favs

---
+++ .history/app/Http/Controllers/InteractionsController_20230606140427.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Http\Request;
+use App\Models\Hall;
+use App\Models\User;
+use App\Models\Admin;
+use App\Models\Comment;
+use App\Models\Like;
+use App\Models\Favourite;
+use App\Http\Traits\GeneralTraits;
+
+
+class InteractionsController extends Controller
+{
+    //
+    use GeneralTraits;
+
+
+    public function addComment(Request $request,  $hall_id)
+    {
+        $thehall = Hall::where('id',$hall_id)->first();
+
+        if($thehall)/* Check if Hall Id is valid */{
+
+
+            $rules = [
+                'content' => 'required|string',
+            ];
+            $validator = Validator::make($request->all(),$rules);
+            if ($validator->fails())/* Check if Comment's content is valid */ {
+                $code = $this->returnCodeAccordingToInput($validator);
+                return $this->returnValidationError($code, $validator);
+            }
+            else{
+            $comment = Comment :: create ([
+                'hall_id'=>$thehall->id,
+                'user_id'=> auth()->user()->id,
+                'content'=>$request->content,
+            ]);
+            $comment->load('users');
+            $comment->load('admins');
+            $comment->load('users');
+            $comment->load('halls');
+            return response()->json(['message' => 'Comment added.']);}
+        }
+        else{
+            response()->json(['message' => 'HALL ID IS WRONG.']);
+        }
+
+
+
+
+
+    }
+
+
+    public function getComment(Request $request,  $hall_id)
+    {
+        $thehall = Hall::where('id',$hall_id)->first();
+        if($thehall){ response()->json(['message' => 'HALL ID.']);
+            $thecomment = Comment::where('hall_id',$hall_id)->first();
+            if($thecomment){
+                $comments = Comment::with(['users' => function($q) {
+                    $q->select('id','email', 'name');}])
+                    ->where('hall_id',$hall_id)->orderBy('id')->get();
+
+                return   response()->json(['message' => 'Comments Successfully Fetched .',
+                                            'data'=> $comments
+                            ],200);
+
+
+        }
+        else{
+            return response()->json(['message' => 'There is no Comments.'],400);
+        }
+
+
+
+
+        }
+        else{
+            return  response()->json(['message' => 'HALL ID IS WRONG.']);
+        }
+
+
+
+    }
+
+
+    public function updateComment(Request $request,  $comment_id)
+    {
+
+        $comments = Comment::with(['users'])
+            ->where('id',$comment_id)->first();
+
+        if ($comments){
+            if ($comments->user_id==$request->user()->id){
+
+                $rules = [
+                    'content' => 'required|string',
+                ];
+                $validator = Validator::make($request->all(),$rules);
+                if ($validator->fails())/* Check if Comment's content is valid */ {
+                    $code = $this->returnCodeAccordingToInput($validator);
+                    return $this->returnValidationError($code, $validator);
+
+                }
+                else{
+                    $comments->Update([
+                        'content'=>$request->content
+                    ]);
+                    return   response()->json(['message' => 'Comments Successfully Updated .',
+                    'data'=> $comments
+                    ],200);
+                }
+            }   else{
+                return  response()->json(['message' => 'Access denied There is no comments here.']);
+            }
+
+
+        }    else{
+            return  response()->json(['message' => 'Comment not found.']);
+        }
+
+    }
+
+    public function deleteComment(Request $request,  $comment_id)
+    {
+            $comments = Comment::with(['users'])
+            ->where('id',$comment_id)->first();
+
+        if ($comments){
+            if ($comments->user_id==$request->user()->id){
+
+
+
+                $comments->delete();
+                return   response()->json(['message' => 'Comments Successfully Deleted .',
+
+                ],200);
+
+
+
+
+            }   else{
+                    return  response()->json(['message' => 'Access denied  here.']);
+                }
+            }    else{
+                return  response()->json(['message' => 'Comment not found.']);
+            }
+
+    }
+
+
+    public function addLike(Request $request,  $hall_id)
+    {
+
+        $thehall = Hall::where('id',$hall_id)->first();
+
+        if($thehall)/* Check if Hall Id is valid */{
+                $like = Like :: create ([
+                    'hall_id'=>$thehall->id,
+                    'user_id'=>  auth()->user()->id,
+                ]);
+                $like->load('users');
+                $like->load('halls');
+        return response()->json(['message' => 'Post liked.']);
+        }
+        else{
+            return $this->removeLike($hall_id);
+        }
+
+
+    }
+
+
+    public function removeLike(Request $request,  $like_id)
+    {
+        $likes = Like::with(['users'])
+        ->where('id',$like_id)->first();
+
+        if($likes)/* Check if Hall Id is valid */{
+            if ($likes->user_id==Auth::guard('user-api')->user()->id){
+                $likes->delete();
+                return   response()->json(['message' => 'likes Successfully Deleted .',
+
+                ],200);
+
+
+            }   else{
+                return  response()->json(['message' => 'Access denied  here.']);
+            }
+        }    else{
+            return  response()->json(['message' => 'like not found.']);
+        }
+
+    }
+
+
+
+
+    public function addFavourite($hall_id)
+{
+    $thehall = Hall::where('id', $hall_id)->where('verified', 'confirmed')->first();
+    $user_id = Auth::guard('user-api')->user()->id;
+
+    if ($thehall) {
+        $fav = Favourite::where('hall_id', $hall_id)->where('user_id', $user_id)->first();
+
+        if (!$fav) {
+            $like = Favourite::create([
+                'hall_id' => $thehall->id,
+                'user_id' => $user_id,
+                'user_name' => Auth::guard('user-api')->user()->name,
+            ]);
+
+            $like->load('users');
+            $like->load('halls');
+
+            return response()->json(['message' => 'Added To Favorites.']);
+        } else {
+            return $this->removeFavourite($hall_id);
+        }
+    } else {
+        return response()->json(['message' => 'HALL ID IS WRONG.'], 404);
+    }
+}
+
+
+
+    public function removeFavourite($hall_id)
+    {
+        $fav = Favourite::with(['users'])
+        ->where('hall_id',$hall_id)->first();
+
+        if($fav)/* Check if Hall Id is valid */{
+            if ($fav->user_id==Auth::guard('user-api')->user()->id){
+                $fav->delete();
+                return   response()->json(['message' => 'UnFavourited Successfully  .',
+
+                ],200);
+
+
+            }   else{
+                return  response()->json(['message' => 'Access denied  here.'],403);
+            }
+        }    else{
+            return  response()->json(['message' => ' not in favourites found.'],404);
+        }
+
+    }
+
+
+
+    public function getFavourite(Request $requestd)
+    {
+
+        $user=Auth::guard('user-api')->user();
+
+
+        $favorites = $user->favourites;
+
+
+        if($favorites){
+
+            return response()->json([
+
+                'message'=>'Returned Successfully ',
+                'favourites'=>$favorites
+
+            ],200);
+
+        }
+
+
+
+
+
+        else{
+            return  response()->json(['message' => 'NOT Found'],404);
+        }
+
+
+
+    }
+
+
+    public function getAllHallsWithIds()
+    {
+        $user=Auth::guard('user-api')->user();
+
+
+        $favorites = $user->favourites;
+        $halls = Hall::with('favourites:id')->get();
+
+        return response()->json([
+            'message' => 'Halls with IDs retrieved successfully.',
+            'halls' => $halls
+        ], 200);
+    }
+
+
+
+
+
+
+
+
+}
---
+++ .history/routes/User/user-routes_20230606140211.php
@@ -0,0 +1,101 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InteractionsController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\Api\User\UserAuthController;
+use App\Http\Controllers\Api\User\UserController;
+use App\Http\Controllers\AuthwController;
+use App\Http\Controllers\hallsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::group([
+    'middleware' => ['api', 'auth.guard:user-api'],
+    'prefix' => 'auth',
+    'namespace' => 'Api',
+
+], function ($router) {
+    Route::group(['namespace' => 'User',], function () {
+        // Route::post('loginUser', [UserAuthController::class, 'login'])->name('login-user');
+        // Route::post('registerUser', [UserAuthController::class, 'userRegister']);
+        Route::get('userProfile', [AuthController::class, 'userProfile']);//1
+        Route::post('updateUser/{user_id}', [AuthController::class, 'updateUser']);//1
+       // Route::post('logoutUser', [UserAuthController::class, 'logout'])->middleware(['auth.guard:user-api']);
+        // Route::get('/getHall/{hall_id}', [UserController::class, 'gethall']); //
+        // Route::get('/getAllHalls', [UserController::class, 'getAllHalls']);
+
+
+        Route::post('bookRoom', [ BookingController::class,'bookRoom']);
+        Route::post('bookPlan', [ BookingController::class,'bookPlan']);
+        Route::post('bookSubService', [ BookingController::class,'bookSubService']);
+
+
+
+        Route::get('/getUserAllBookings', [BookingController::class, 'getUserAllBookings']);//1
+        Route::get('/getUserConfirmedBookings', [BookingController::class, 'getUserConfirmedBookings']);//1
+        Route::get('/getUserUnConfirmedBookings', [BookingController::class, 'getUserUnConfirmedBookings']);//1
+        Route::get('/getUserCancelledBookings', [BookingController::class, 'getPlannerCancelledBookings']);//1
+
+        Route::get('/getUserAllPlanRequests', [BookingController::class, 'getUserAllPlanRequests']);//1
+        Route::get('/getUserConfirmedPlanRequests', [BookingController::class, 'getUserConfirmedPlanRequests']);//1
+        Route::get('/getUserUnConfirmedPlanRequests', [BookingController::class, 'getUserUnConfirmedPlanRequests']);//1
+        Route::get('/getUserCancelledPlanRequests', [BookingController::class, 'getPlannerCancelledPlanRequests']);//1
+
+        Route::get('/getUserAllSubRequests', [BookingController::class, 'getUserAllSubRequests']);//1
+        Route::get('/getUserConfirmedSubRequests', [BookingController::class, 'getUserConfirmedSubRequests']);//1
+        Route::get('/getUserUnConfirmedSubRequests', [BookingController::class, 'getUserUnConfirmedSubRequests']);//1
+        Route::get('/getUserCancelledSubRequests', [BookingController::class, 'getPlannerCancelledSubRequests']);//1
+
+
+
+        Route::post('addFavourite/{hall_id}', [InteractionsController::class, 'addFavourite']);
+        Route::delete('removeFavourite/{hall_id}', [InteractionsController::class, 'removeFavourite']);
+        Route::get('getFavourite', [InteractionsController::class, 'getFavourite']);
+
+        Route::get('getAllHallsWithIds', [InteractionsController::class, 'getAllHallsWithIds']);
+
+
+
+        Route::post('halls/{hall_id}/addLike', [InteractionsController::class, 'addLike']);
+        Route::post('halls/{hall_id}/addComment', [InteractionsController::class, 'addComment']);
+        Route::get('halls/{hall_id}/getComment', [InteractionsController::class, 'getComment']);
+        Route::post('halls/{comment_id}/updateComment', [InteractionsController::class, 'updateComment']);
+        Route::delete('halls/{comment_id}/deleteComment', [InteractionsController::class, 'deleteComment']);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::group([
+    'middleware' => ['api'],
+    'prefix' => 'auth',
+    'namespace' => 'Api',
+
+], function ($router) {
+    Route::group(['namespace' => 'User',], function () {
+        Route::get('userphoto/{user_id}', [AuthController::class, 'getUserPhoto']);
+    });
+});
+
+Route::any('{url}',function (){
+    return response()->json('this url not found', 401);
+})->where('url','.*')->middleware('api');
